b6d779fa328ebb7c147452d081b01e944bc15811
Erryza Nur Alif <erryzanuralif@gmail.com>
2022-11-27 12:49:45 +0700

    assertions

---
+++ cypress/e2e/assertions.cy.js
@@ -0,0 +1,16 @@
+describe("My Second Test", () => {
+  it('Clicking "type" shows th eright headings', () => {
+    cy.visit("https://example.cypress.io");
+
+    cy.pause();
+
+    cy.contains("type").click();
+
+    // Should be on a new URL which includes "/commands/actions"
+
+    // Get an input, type into it and verify that the value has been updated
+    cy.get(".action-email")
+      .type("usertest@gmail.com")
+      .should("have.value", "usertest@gmail.com");
+  });
+});

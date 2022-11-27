4348b7ce47b319f949cf14eef325200764bd2526
Erryza Nur Alif <erryzanuralif@gmail.com>
2022-11-27 12:53:15 +0700

    update script assertions

--- cypress/e2e/assertions.cy.js
+++ cypress/e2e/assertions.cy.js
@@ -7,6 +7,7 @@ describe("My Second Test", () => {
     cy.contains("type").click();
 
     // Should be on a new URL which includes "/commands/actions"
+    cy.url().should("include", "/commands/actions");
 
     // Get an input, type into it and verify that the value has been updated
     cy.get(".action-email")
